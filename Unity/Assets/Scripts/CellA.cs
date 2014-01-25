a9a9fbfb2aae4f6ec2b5319e92986df22ca3b5f3
Almuva <almuva1510@gmail.com>
2014-01-25 21:08:07 +0100

    -pfff ya no me acuerdo

---
+++ Unity/Assets/Scripts/CellA.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CellA : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void OnCollisionEnter(Collision c)
+	{
+		Debug.Log ("Ouch! " + this.gameObject.name + " - " + c.gameObject.name);
+	}
+
+	public void Step(PlayerA.TEAM team)
+	{
+		switch (team)
+		{
+		case PlayerA.TEAM.BLUE:
+			renderer.material.color = Color.blue;
+			break;
+		case PlayerA.TEAM.GREEN:
+			renderer.material.color = Color.green;
+			break;
+		case PlayerA.TEAM.RED:
+			renderer.material.color = Color.red;
+			break;
+		case PlayerA.TEAM.YELLOW:
+			renderer.material.color = Color.yellow;
+			break;
+		}
+	}
+
+	public void Clear()
+	{
+		renderer.material.color = Color.white;
+	}
+
+
+}
--- Unity/Assets/Scripts/PlayerA.cs
+++ Unity/Assets/Scripts/PlayerA.cs
@@ -2,19 +2,26 @@
 using System.Collections;
 
 public class PlayerA : MonoBehaviour {
+	public enum TEAM
+	{
+		BLUE = 1,
+		RED = 2,
+		GREEN = 3,
+		YELLOW = 4
+	};
+
 	public float maxSpeed = 1;
 	public float accelerationFactor = 0.9f;
 	public Vector3 desiredSpeed = Vector3.zero;
 	public Vector3 currentSpeed = Vector3.zero;
 	Vector3 shootDir;
 
-	public bool A, B, X, Y, StartB, SelectB, LB, RB;
-	public float LT, RT, padX, padY;
-
 	CharacterController controller;
 
 	public float shootForce = 10;
 	public GameObject bullet;
+
+	public TEAM team = TEAM.BLUE; 
 
 	void Awake()
 	{
@@ -23,7 +30,24 @@ public class PlayerA : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		controller.detectCollisions = true;
 		
+		switch (team)
+		{
+		case TEAM.BLUE:
+			renderer.material.color = Color.blue;
+			break;
+		case TEAM.GREEN:
+			renderer.material.color = Color.green;
+			break;
+		case TEAM.RED:
+			renderer.material.color = Color.red;
+			break;
+		case TEAM.YELLOW:
+			renderer.material.color = Color.yellow;
+			break;
+			
+		}
 	}
 	
 	// Update is called once per frame
@@ -31,65 +55,15 @@ public class PlayerA : MonoBehaviour {
 		desiredSpeed = Vector3.zero;
 
 	//MOVEMENT
-		//KEYBOARD
-		if (Input.GetKey(KeyCode.A))
-		{
-			desiredSpeed.x = -1;
-		}
-		else if (Input.GetKey(KeyCode.D))
-		{
-			desiredSpeed.x = 1;
-		}
-		if (Input.GetKey(KeyCode.W))
-		{
-			desiredSpeed.z = 1;
-		}
-		else if (Input.GetKey(KeyCode.S))
-		{
-			desiredSpeed.z = -1;
-		}
-
 		//CONTROLLER
 		desiredSpeed.x = Input.GetAxis("C1_LeftX");
 		desiredSpeed.z = Input.GetAxis("C1_LeftY");
-
-		A = Input.GetButton ("C1_A");
-		B = Input.GetButton ("C1_B");
-		X = Input.GetButton ("C1_X");
-		Y = Input.GetButton ("C1_Y");
-		LB = Input.GetButton ("C1_LB");
-		RB = Input.GetButton ("C1_RB");
-		StartB = Input.GetButton ("C1_Start");
-		SelectB = Input.GetButton ("C1_Select");
-
-		padX = Input.GetAxis("C1_DPadX");
-		padY = Input.GetAxis("C1_DPadY");
-		LT = Input.GetAxis ("C1_Triggers");
-
-
-
 		desiredSpeed.Normalize ();
 		desiredSpeed *= maxSpeed;
 		currentSpeed = currentSpeed * accelerationFactor + desiredSpeed * (1-accelerationFactor);
 		controller.SimpleMove(currentSpeed);
 
 	//SHOOT
-		//KEYBOARD
-		Vector3 mousePos = Input.mousePosition;
-		mousePos.z = 30.0f;
-		mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-		shootDir = mousePos - transform.position;
-		shootDir.y = 0;
-		shootDir.Normalize();
-
-		if (Input.GetMouseButtonDown(0))
-		{
-			GameObject b = GameObject.Instantiate(bullet, transform.position + shootDir, Quaternion.identity) as GameObject;
-			//b.GetComponent<Bullet>().direction = shootDir;
-			b.rigidbody.AddForce(shootDir * shootForce, ForceMode.Impulse);
-			//Debug.Log(shootDir);
-		}
-
 		//CONTROLLER
 		shootDir.x = Input.GetAxis ("C1_RightX");
 		shootDir.z = -Input.GetAxis ("C1_RightY");
@@ -104,7 +78,41 @@ public class PlayerA : MonoBehaviour {
 			//Debug.Log(shootDir);
 		}
 
+		CellA cell = Level.instance.GetCell(GetCellPos());
+		if (IsSharingCell()) 
+		{
+			cell.Clear();
+		}
+		else 
+		{
+			cell.Step(team);
+		}
+	}
 
-		Debug.DrawRay(transform.position,shootDir,Color.red);
+	void OnCollisionEnter(Collision c)
+	{
+		Debug.Log (this.gameObject.name + " - " + c.gameObject.name);
+	}
+	
+	bool IsSharingCell()
+	{
+		GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+		Vector2 pos = GetCellPos();
+		foreach (GameObject g in players)
+		{
+			if (g == gameObject) continue;
+			Vector2 pos2 = g.GetComponent<Player>().GetCellPos();
+			if (pos.x == pos2.x && pos.y == pos2.y) 
+			{
+				Debug.Log ("Sharing cell " + pos.x + "," + pos.y);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public Vector2 GetCellPos()
+	{
+		return new Vector2((int)transform.position.z,(int)transform.position.x);
 	}
 }
